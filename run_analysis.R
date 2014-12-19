d0db4315e4dcea7d279c268717c2d5c45730fc9b
Corragon <corragon@gmail.com>
2014-12-18 21:28:23 -0600

    Mostly finished the run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 ## Preparatory work: Loading data 
 test <- read.table("data/test/X_test.txt")
-subject_test <- read.table("data/test/subject_test.txt")
-activity_test <- read.table("data/test/y_test.txt")
+subject_test <- read.table("data/test/subject_test.txt", col.names = "subjectID")
+activity_test <- read.table("data/test/y_test.txt", col.names = "activity")
 test <- cbind(subject_test, activity_test, test)
 
 train <- read.table("data/train/X_train.txt")
-subject_train <- read.table("data/train/subject_train.txt")
-activity_train <- read.table("data/train/y_train.txt")
+subject_train <- read.table("data/train/subject_train.txt", col.names = "subjectID")
+activity_train <- read.table("data/train/y_train.txt", col.names = "activity")
 train <- cbind(subject_train, activity_train, train)
 
 # Cleanup temp variables
@@ -16,6 +16,8 @@ rm(subject_test, activity_test, subject_train, activity_train)
 
 ## Step 1: Merge sets into one
 data <- rbind(train, test)
+dataNames <- names(data)
+data <- data[order(data$subjectID),]
 
 # Cleanup from step 1
 rm(train, test)
@@ -26,22 +28,31 @@ rm(train, test)
 features <- read.table("data/features.txt",
                        colClasses=c("numeric", "character"))
 # We want to make sure our added columns are included in the pattern match
-features <- rbind(c(NA, "subjectID-not-std"),
-                  c(NA, "activity-not-std"), 
+features <- rbind(c(NA, "subjectID"),
+                  c(NA, "activity"), 
                   features)
 
 # Get column index of every mean or std measurement (plus our added cols)
 # Note: Does NOT include *-meanFreq() or angle(X, gravityMean) columns
-columnsToExtract <- grep("(mean|std)(?!F)", features$V2, perl=T)
+columnsToExtract <- grep("(mean|std|subject|activity)(?!F)", features$V2, perl=T)
 data <- data[,columnsToExtract]
 
 
-# Step 3 or something
+# Step 3
 activityLevels <- read.table("data/activity_labels.txt")
 
+data$activity <- factor(data$activity)
+levels(data$activity) <- activityLevels$V2
+
+rm(activityLevels)
+
+## Step 4
+cleanedNames <- features$V2[columnsToExtract]
+cleanedNames <- gsub("^t", "time", cleanedNames)
+cleanedNames <- gsub("^f", "freq", cleanedNames)
+cleanedNames <- gsub("\\(\\)", "", cleanedNames)
+names(data) <- cleanedNames
 
 
-## Step 4?
-names(data) <- c("subject", features$V2)
-
-
+## Step 5
+tidyData <- aggregate(x=data[,3:68], by=list(data$subjectID, data$activity), FUN = mean)
